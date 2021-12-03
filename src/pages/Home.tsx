6d3bac4eb5ce632680b94987e77347f927055a10
aiibe <souksavanh.sy@gmail.com>
2021-12-03 10:37:17 +0100

    Remove react-select and use react-form-hook

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,17 @@
-import { useMemo, useState } from "react";
-import Select from "react-select";
+import { useMemo } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 import { departmentOptions, states } from "../constants";
-import { OptionType } from "../types/states";
+import { FormInputs } from "../types/form";
 import { stateToOption } from "../utils";
 
 function Home() {
   const memStates = useMemo(() => stateToOption(states), [states]);
-  const [selectedState, setSelectedState] = useState<OptionType | null>(
-    memStates[0]
-  );
-  const [selectedDepartment, setSelectedDepartment] =
-    useState<OptionType | null>(departmentOptions[0]);
+  const { register, handleSubmit } = useForm<FormInputs>();
+
+  const onSubmit: SubmitHandler<FormInputs> = (data) => {
+    console.log(data);
+  };
 
   return (
     <>
@@ -18,49 +19,54 @@ function Home() {
         <h1>HRnet</h1>
       </div>
       <div className="container">
-        <a href="employee-list.html">View Current Employees</a>
+        <Link to="employee-list">View Current Employees</Link>
         <h2>Create Employee</h2>
-        <form>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
           <label>First Name</label>
-          <input type="text" />
+          <input type="text" {...register("firstName")} />
 
           <label>Last Name</label>
-          <input type="text" />
+          <input type="text" {...register("lastName")} />
 
           <label>Date of Birth</label>
-          <input type="text" />
+          <input type="text" {...register("birthDate")} />
 
           <label>Start Date</label>
-          <input type="text" />
+          <input type="text" {...register("startDate")} />
 
           <fieldset className="address">
             <legend>Address</legend>
 
             <label>Street</label>
-            <input type="text" />
+            <input type="text" {...register("streetLocation")} />
 
             <label>City</label>
-            <input type="text" />
+            <input type="text" {...register("cityLocation")} />
 
             <label>State</label>
-            <Select
-              value={selectedState}
-              options={memStates}
-              onChange={(opt) => setSelectedState(opt)}
-            />
+            <select {...register("stateLocation")}>
+              {memStates.map(({ value, label }) => (
+                <option key={value} value={value}>
+                  {label}
+                </option>
+              ))}
+            </select>
 
             <label>Zip Code</label>
-            <input type="number" />
+            <input type="number" {...register("zipCode")} />
           </fieldset>
 
           <label>Department</label>
-          <Select
-            value={selectedDepartment}
-            options={departmentOptions}
-            onChange={(opt) => setSelectedDepartment(opt)}
-          />
+          <select {...register("department")}>
+            {departmentOptions.map(({ value, label }) => (
+              <option key={value} value={value}>
+                {label}
+              </option>
+            ))}
+          </select>
+          <button type="submit">Save</button>
         </form>
-        <button>Save</button>
       </div>
       <div id="confirmation" className="modal">
         Employee Created!
---
+++ src/types/form.ts
@@ -0,0 +1,11 @@
+export type FormInputs = {
+  firstName: string;
+  lastName: string;
+  birthDate: string;
+  startDate: string;
+  streetLocation: string;
+  cityLocation: string;
+  stateLocation: string;
+  zipCode: number;
+  department: string;
+};
