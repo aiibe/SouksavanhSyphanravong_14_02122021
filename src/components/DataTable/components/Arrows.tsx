1a07a1a767c87d1c6d85f573bec68af9f769d396
aiibe <souksavanh.sy@gmail.com>
2021-12-14 14:59:14 +0100

    Fix icons sort

--- src/components/DataTable/components/Arrows.tsx
+++ src/components/DataTable/components/Arrows.tsx
@@ -5,11 +5,11 @@ function Arrows({ ascending, active }: PropType) {
     <span className="react-datatable__arrow-box">
       <i
         className="react-datatable__arrow react-datatable__arrow-up"
-        style={{ opacity: active ? (ascending ? 1 : 0.2) : 1 }}
+        style={{ opacity: active ? (ascending ? 1 : 0.2) : 0.2 }}
       ></i>
       <i
         className="react-datatable__arrow react-datatable__arrow-down"
-        style={{ opacity: active ? (ascending ? 0.2 : 1) : 1 }}
+        style={{ opacity: active ? (ascending ? 0.2 : 1) : 0.2 }}
       ></i>
     </span>
   );
